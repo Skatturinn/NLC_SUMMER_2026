5c355ad696288a7cec612b3fae4ff020f7412369
Skatturinn <elias.ludviksson@gmail.com>
2026-09-16 19:50:10 -0400

    slow down

--- 260916_ri_test_2.cpp
+++ 260916_ri_test_2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <chrono>
-// #include <thread>
+#include <thread>
 // #include <vector>
 // #include <iomanip>
 
@@ -24,6 +24,8 @@ int main() {
 		double dt = std::chrono::duration<double>(now - last).count();
 		std::cout << "\rCurrent i: " << i << "; " << "Elapsed time: " << dt << "s;" << std::flush;
 		last = now;
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
 
 	std::cout << "\n Lok." << std::endl;
