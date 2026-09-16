e3db73d228edce0d43ba92afcc8a82d5b6c7c2bd
Skatturinn <elias.ludviksson@gmail.com>
2026-09-16 19:48:07 -0400

    realtimee cout logging test

---
+++ 260916_ri_test_2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <chrono>
+// #include <thread>
+// #include <vector>
+// #include <iomanip>
+
+
+// #include "RobotInterface.hpp"
+
+int main() {
+
+	// RobotInterface robot_api;
+
+	// if (robot_api.Start() != 0) {
+	// 	std::cerr << "Failed to start RobotInterface. Exiting.\n";
+	// 	return 1;
+	// }
+	// std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	// std::chrono::milliseconds(1000);
+	auto last = std::chrono::steady_clock::now();
+
+	for (int i = 0; i < 100; ++i) {
+		auto now = std::chrono::steady_clock::now();
+		double dt = std::chrono::duration<double>(now - last).count();
+		std::cout << "\rCurrent i: " << i << "; " << "Elapsed time: " << dt << "s;" << std::flush;
+		last = now;
+	}
+
+	std::cout << "\n Lok." << std::endl;
+
+	return 0;
+}
